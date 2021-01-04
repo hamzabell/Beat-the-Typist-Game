3c730de22accde588e57bedacf3c5d77c4e3e027
hamzabell <akanbassey8@gmail.com>
2021-01-04 18:21:30 +0100

    Navigation and basse style components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Game from "./pages/Game";
+import HighScores from "./pages/HighScores";
+import GameOver from "./pages/GameOver";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import { Container, Main, Global } from "./styled";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Global>
+        <Main>
+          <Container>
+            <Navbar />
+            <Switch>
+              <Route path="/game" component={Game} />
+              <Route path="/highScores" component={HighScores} />
+              <Route path="/gameOver" component={GameOver} />
+              <Route path="/" exact component={Home} />
+            </Switch>
+          </Container>
+        </Main>
+      </Global>
+    </Router>
   );
 }
 
---
+++ src/styled/index.js
@@ -0,0 +1,4 @@
+import Container from "./Container";
+import Main from "./Main";
+import Global from "./global";
+export { Container, Main, Global };
