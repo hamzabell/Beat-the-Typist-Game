a0fa8ce6dddec35944c29db116b80b1fccd4c6ca
hamzabell <akanbassey8@gmail.com>
2021-01-10 16:46:26 +0100

    persisting of theme preference

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -3,8 +3,19 @@ import { useEffect, useState } from "react";
 export default () => {
   const [theme, setTheme] = useState("light");
 
+  useEffect(() => {
+    const localStorageTheme = window.localStorage.getItem("theme");
+    setTheme(localStorageTheme || "light");
+  }, []);
+
   const toggleTheme = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+    }
   };
 
   return [theme, toggleTheme];
